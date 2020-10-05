c43c1d50309967278a41413de9b258a6b416ea6d
archena <archena@users.noreply.github.com>
2020-10-05 20:37:47 +0100

    Getting Tensorflow Lite to compile and link in PlatformIO / Arduino project

---
+++ firmware/lib_setup.sh
@@ -0,0 +1,5 @@
+wget http://downloads.arduino.cc/libraries/github.com/bcmi-labs/Arduino_TensorFlowLite-2.1.0-ALPHA-precompiled.zip
+unzip Arduino_TensorFlowLite-2.1.0-ALPHA-precompiled.zip
+mkdir lib
+mv Arduino_TensorFlowLite-2.1.0-ALPHA-precompiled lib
+rm Arduino_TensorFlowLite-2.1.0-ALPHA-precompiled.zip
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,7 +1,20 @@
+#include <Arduino.h>
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+
+#define LED 13
+
 void setup() {
-  // TODO
+  //Serial.begin(9600);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // TODO
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(250);
 }
